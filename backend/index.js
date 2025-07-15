8486acc18a21b759c0b295f823c32974e8eb537a
Villiers Ng <villiersng@gmail.com>
2025-07-15 14:06:50 +0530

    mongo

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,14 @@
+import dotenv from "dotenv";
+dotenv.config({});
 import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-import dotenv from "dotenv";
 import connectDB from "./utils/db.js";
 import userRoute from "./routes/user.route.js";
 import companyRoute from "./routes/company.route.js";
 import jobRoute from "./routes/job.route.js";
 import applicationRoute from "./routes/application.route.js";
 
-dotenv.config({});
 
 const app = express();
 
